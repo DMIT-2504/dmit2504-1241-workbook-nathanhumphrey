ef2428c5c3e9dffdbac5c77585f54d4df83b16bf
Nathan Humphrey <NathanH@nait.ca>
2024-09-11 08:16:23 -0600

    Week two async complete

--- week_02_async_dart/bin/week_02_async_dart.dart
+++ week_02_async_dart/bin/week_02_async_dart.dart
@@ -1,10 +1,17 @@
 import 'package:week_02_async_dart/week_02_async_dart.dart';
 
 Future<void> main(List<String> arguments) async {
-  // Get a random dog image
+  // Fetch from the dog api
+  // https://dog.ceo/api/breeds/image/random
+  // https://dog.ceo/api/breeds/list/all
+
+  // Get random dog image
   const dogUrl = 'https://dog.ceo/api/breeds/image/random';
   var dogData = await getJson(dogUrl);
-  print(dogData['message']);
+  print(dogData);
 
-  // TODO: fetch data from the breeds endpoint
+  // Get all breeds
+  const allBreedsUrl = 'https://dog.ceo/api/breeds/list/all';
+  dogData = await getJson(allBreedsUrl);
+  print(dogData['message']['australian'][0]); // shepherd
 }
--- week_02_async_dart/lib/week_02_async_dart.dart
+++ week_02_async_dart/lib/week_02_async_dart.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-///
+/// Returns JSON from the [endpoint]
 Future<dynamic> getJson(String endpoint) async {
   final url = Uri.parse(endpoint);
   final response = await http.get(url);
