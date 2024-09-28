e425dbb8f008b863a825fefbaf7241098e4c87d0
Nathan Humphrey <nathpaulhump@gmail.com>
2024-09-27 18:00:06 -0600

    Week 04 Form Validation 3

--- week_04_text_fields/lib/main.dart
+++ week_04_text_fields/lib/main.dart
@@ -14,46 +14,70 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Mobile UI Form'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            child: Column(
-              children: [
-                const Text(
-                  'New User Form',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                // Username cannot be empty
-                TextFormField(
-                  validator: (value) => value == null || value.trim() == ''
-                      ? 'Username cannot be empty'
-                      : null,
-                  decoration: const InputDecoration(
-                    label: Text('Username'),
-                  ),
-                ),
-                TextFormField(
-                  validator: (value) => value == null || value.trim() == ''
-                      ? 'Password cannot be empty'
-                      : null,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    label: Text('Password'),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Sign Up'),
-                ),
-              ],
+        body: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: UserSignUpForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class UserSignUpForm extends StatefulWidget {
+  const UserSignUpForm({super.key});
+
+  @override
+  State<UserSignUpForm> createState() => _UserSignUpFormState();
+}
+
+class _UserSignUpFormState extends State<UserSignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          const Text(
+            'New User Form',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ),
+          // Username cannot be empty
+          TextFormField(
+            validator: (value) => value == null || value.trim() == ''
+                ? 'Username cannot be empty'
+                : null,
+            decoration: const InputDecoration(
+              label: Text('Username'),
+            ),
+          ),
+          TextFormField(
+            validator: (value) => value == null || value.trim() == ''
+                ? 'Password cannot be empty'
+                : null,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              label: Text('Password'),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              // test validation on fields
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Processing signup'),
+                ));
+              }
+            },
+            child: const Text('Sign Up'),
+          ),
+        ],
       ),
     );
   }
