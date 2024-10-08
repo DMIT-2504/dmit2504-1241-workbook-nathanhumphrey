d8b9d3e1d04fde73787d816d1d47fef64ddc6ff2
Nathan Humphrey <NathanH@nait.ca>
2024-10-07 18:13:10 -0600

    Week 06 Shared Prefs

--- week_06_kv_file_storage/lib/pages/shared_prefs.dart
+++ week_06_kv_file_storage/lib/pages/shared_prefs.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SharedPrefs extends StatelessWidget {
+class SharedPrefs extends StatefulWidget {
   const SharedPrefs({super.key});
+
+  @override
+  State<SharedPrefs> createState() => _SharedPrefsState();
+}
+
+class _SharedPrefsState extends State<SharedPrefs> {
+  String _message = '';
+
+  @override
+  void initState() {
+    _readFromStorage();
+    super.initState();
+  }
+
+  /// Get the k,v value, if any
+  Future<void> _readFromStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _message = prefs.getString('message') ?? '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +31,38 @@ class SharedPrefs extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Shared Preferences'),
       ),
-      body: const Center(
-        child: Text('CONTENT'),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                setState(() {
+                  _message = 'Some message to write';
+                });
+
+                final prefs = await SharedPreferences.getInstance();
+                prefs.setString('message', _message);
+              },
+              child: const Text('Save Message'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                setState(() {
+                  _message = '';
+                });
+
+                final prefs = await SharedPreferences.getInstance();
+                prefs.clear();
+                // Or remove single items
+                // prefs.remove('message');
+              },
+              child: const Text('Delete Message'),
+            ),
+            _message.isNotEmpty
+                ? Text(_message)
+                : const Text('Nothing to read'),
+          ],
+        ),
       ),
     );
   }
